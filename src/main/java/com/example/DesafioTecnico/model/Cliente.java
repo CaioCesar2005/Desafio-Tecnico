125f43e53bc15a215ba3afc15c08c8ff850a2577
CaioCesar2005 <159182862+CaioCesar2005@users.noreply.github.com>
2025-06-22 20:09:27 -0300

    feat (model): implementar validação com Bean Validation em Cliente

--- src/main/java/com/example/DesafioTecnico/model/Cliente.java
+++ src/main/java/com/example/DesafioTecnico/model/Cliente.java
@@ -1,6 +1,8 @@
 package com.example.DesafioTecnico.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,25 +10,33 @@ import lombok.Setter;
 @Table(name = "clientes")
 @Getter
 @Setter
+
+
 public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Ö nome é obrigatório")
     @Column(nullable = false)
     private String nome;
 
+    @NotBlank(message = "Ö CPF é obrigatório")
+    @Pattern(regexp = "\\d{11}", message = "O CPF deve conter 11 dígitos numéricos.")
     @Column(nullable = false, unique = true)
     private String cpf;
 
     private String telefone;
 
     private String email;
+    
 
     public Cliente() {}
 
-    public Cliente(String nome, String cpf, String telefone, String email) {
+
+    public Cliente(Long id, String nome, String cpf, String telefone, String email) {
+        this.id = id;
         this.nome = nome;
         this.cpf = cpf;
         this.telefone = telefone;
