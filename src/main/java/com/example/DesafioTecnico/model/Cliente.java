b8ceb89f9a644b21bbcf3b1c6b5c0168856ed243
CaioCesar2005 <159182862+CaioCesar2005@users.noreply.github.com>
2025-06-22 20:01:11 -0300

    chore (model): criar entidade Cliente

---
+++ src/main/java/com/example/DesafioTecnico/model/Cliente.java
@@ -0,0 +1,35 @@
+package com.example.DesafioTecnico.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clientes")
+@Getter
+@Setter
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private String telefone;
+
+    private String email;
+
+    public Cliente() {}
+
+    public Cliente(String nome, String cpf, String telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+    }
+}
