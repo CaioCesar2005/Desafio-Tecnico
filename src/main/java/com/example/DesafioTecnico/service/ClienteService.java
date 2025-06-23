5fa950f2953a984be0653b364ae4566a7d4c8601
CaioCesar2005 <159182862+CaioCesar2005@users.noreply.github.com>
2025-06-23 19:47:23 -0300

    feat (service): implementar servico de excluir cliente (cancesar suas contas automaticamente)

--- src/main/java/com/example/DesafioTecnico/service/ClienteService.java
+++ src/main/java/com/example/DesafioTecnico/service/ClienteService.java
@@ -1,7 +1,10 @@
 package com.example.DesafioTecnico.service;
 
 import com.example.DesafioTecnico.model.Cliente;
+import com.example.DesafioTecnico.model.Conta;
+import com.example.DesafioTecnico.model.SituacaoConta;
 import com.example.DesafioTecnico.repository.ClienteRepository;
+import com.example.DesafioTecnico.repository.ContaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +14,12 @@ import java.util.List;
 public class ClienteService {
 
     private final ClienteRepository clienteRepository;
+    private final ContaRepository contaRepository;
+    
 
-    public ClienteService(ClienteRepository clienteRepository) {
+    public ClienteService(ClienteRepository clienteRepository, ContaRepository contaRepository) {
         this.clienteRepository = clienteRepository;
+        this.contaRepository = contaRepository;    
     }
 
     @Transactional
@@ -41,4 +47,22 @@ public class ClienteService {
     public List<Cliente> listarClientes() {
         return clienteRepository.findAll();
     }
+
+    @Transactional
+    public void excluirCliente(Long id) {
+        Cliente cliente = clienteRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado"));
+
+        List<Conta> contas = contaRepository.findByCliente(cliente);
+
+        for (Conta conta : contas) {
+            if (conta.getSituacao() != SituacaoConta.CANCELADA) {
+                conta.setSituacao(SituacaoConta.CANCELADA);
+            }
+        }
+
+        contaRepository.saveAll(contas);
+        clienteRepository.delete(cliente);
+    }
+
 }
