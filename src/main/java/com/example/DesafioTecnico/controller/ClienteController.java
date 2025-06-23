bd332b50b5ff6ff0096a85d5b963b3a3fd53b1fb
CaioCesar2005 <159182862+CaioCesar2005@users.noreply.github.com>
2025-06-23 20:28:14 -0300

    feat (controller): criar ClienteController com endpoints necessarios

---
+++ src/main/java/com/example/DesafioTecnico/controller/ClienteController.java
@@ -0,0 +1,43 @@
+package com.example.DesafioTecnico.controller;
+
+import com.example.DesafioTecnico.model.Cliente;
+import com.example.DesafioTecnico.model.Conta;
+import com.example.DesafioTecnico.service.ClienteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService){
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente) {
+        Cliente clienteNovo = clienteService.cadastrarCliente(cliente);
+        return ResponseEntity.ok(clienteNovo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente){ 
+        Cliente clienteAtualizado = clienteService.atualizarCliente(id, cliente);
+        return ResponseEntity.ok(clienteAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirCliente(@PathVariable Long id) {
+        clienteService.excluirCliente(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}/contas")
+    public ResponseEntity<List<Conta>> listarContasDoCliente(@PathVariable Long id) {
+        List<Conta> contas = clienteService.listarContasDoCliente(id);
+        return ResponseEntity.ok(contas);
+    }
+}
