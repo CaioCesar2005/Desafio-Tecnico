65fe32d5275920134ca5d223b51128c7d131a373
unknown <caio13483@gmail.com>
2025-06-26 15:43:42 -0300

    docs (repository): Adcionar comentarios no codigo

--- src/main/java/com/example/DesafioTecnico/repository/ClienteRepository.java
+++ src/main/java/com/example/DesafioTecnico/repository/ClienteRepository.java
@@ -4,6 +4,7 @@ import com.example.DesafioTecnico.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+// Interface para operações de acesso a dados da entidade Cliente
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     
     Optional<Cliente> findByCpf(String cpf);
--- src/main/java/com/example/DesafioTecnico/repository/ContaRepository.java
+++ src/main/java/com/example/DesafioTecnico/repository/ContaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+// Interface para operações de acesso a dados da entidade Conta
 public interface ContaRepository extends JpaRepository<Conta, Long> {
 
     List<Conta> findByCliente(Cliente cliente);
